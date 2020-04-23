9188ba3c516cef4b3a5474a71c4814513b78193c
john.ochoa <7806286>
2020-04-23 10:48:29 -0500

    primer commit

---
+++ src/test/java/com/comercioelectronico/co/demo/ComercioelectronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comercioelectronico.co.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercioelectronicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
